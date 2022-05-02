2b2af945b744ff196c6af917da5234a14baa6e2d
vlkishore <85967064+vlkishore@users.noreply.github.com>
2022-05-02 23:57:10 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = "us-east-1"  
-  access_key = "AKIAZVEAOU24TC3JITIM"
-  secret_key =  "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"
+  access_key = <>
+  secret_key =  <>
   #assume_role {
     #role_arn    = "arn:aws:iam::663841842873:role/tf_role"
     #external_id = null
