3b24bc369a7cce3db16aed20b5abc25995ea1185
Kishore <kishorekrvl@gmail.com>
2022-05-09 19:27:07 +0100

    new key pair

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "instance" {
   ami = "ami-03ededff12e34e59e"
   instance_type = var.instance_type
-  key_name = "mykey"  
+  key_name = "newkeypair"  
   tags = { 
      Name = "tf-instance1"
 }
