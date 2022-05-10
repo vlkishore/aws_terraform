f34e6b1c23781440b8744b1be0c923af7222e3f5
Kishore <kishorekrvl@gmail.com>
2022-05-10 11:01:16 +0100

    security group tf

---
+++ security_group.tf
@@ -0,0 +1,30 @@
+resource "aws_security_group" "security_port" {
+  name        = "security_port"
+  description = "security group for instance"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+ ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+ # outbound from server
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags= {
+    Name = "security_port"
+  }
+}
