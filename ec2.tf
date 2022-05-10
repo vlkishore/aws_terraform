57fab3273d9719c04f6a2abe6bd7d0dbe358c407
Kishore <kishorekrvl@gmail.com>
2022-05-10 11:04:20 +0100

    security groups attribute

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "instance" {
   ami = "ami-03ededff12e34e59e"
   instance_type = var.instance_type
   key_name = "newkeypair"  
-  security_groups= aws_security_group.security_port.id
+  security_groups= ["security_port"]
   tags = { 
      Name = "tf-instance1"
 }
