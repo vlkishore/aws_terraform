048f70b8f28bea6cfac5449eedbd0a1735d458f1
Kishore <kishorekrvl@gmail.com>
2022-05-10 10:25:37 +0100

    new security group

--- ec2.tf
+++ ec2.tf
@@ -2,6 +2,7 @@ resource "aws_instance" "instance" {
   ami = "ami-03ededff12e34e59e"
   instance_type = var.instance_type
   key_name = "newkeypair"  
+  security_groups= ["security_port"]
   tags = { 
      Name = "tf-instance1"
 }
